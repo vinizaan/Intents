1ef1486f9d3b55f79539b0cfe6762486e01dd2f4
vinizaan <viniciuszanfelici@hotmail.com>
2023-11-07 20:13:29 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
@@ -0,0 +1,33 @@
+package br.edu.scl.ifsp.sdm.intents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
+import br.edu.scl.ifsp.sdm.intents.databinding.ActivityParameterBinding
+
+class ParameterActivity : AppCompatActivity() {
+    private val activityParameterBinding: ActivityParameterBinding by lazy {
+        ActivityParameterBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityParameterBinding.root)
+        setSupportActionBar(activityParameterBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = localClassName
+
+        intent.getStringExtra(PARAMETER_EXTRA)?.let {
+            activityParameterBinding.parameterEt.setText(it)
+        }
+
+        activityParameterBinding.apply {
+            returnCloseBt.setOnClickListener{
+                val resultIntent = Intent().apply {
+                    putExtra(PARAMETER_EXTRA, parameterEt.text.toString())
+                }
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+}
